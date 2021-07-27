60c927035cfc7e80ca9e1ccd3ddf1a771e896c63
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2021-07-26 23:44:29 -0300

    const prod = 'https://aux-sdp-api.herokuapp.com//api/v1'

--- src/helpers/scope.js
+++ src/helpers/scope.js
@@ -1,5 +1,6 @@
 
-const prod = 'https://api-sdp.herokuapp.com/api/v1'
+// const prod = 'https://api-sdp.herokuapp.com/api/v1'
+const prod = 'https://aux-sdp-api.herokuapp.com//api/v1'
 const localhost = 'http://localhost:5000/api/v1'
 
 const queryString = window.location.search
