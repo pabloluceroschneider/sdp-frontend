c33f75efd655812a6f90b9a23516cb00e9362983
pabloluceroschneider <pabloluceroschneider@gmail.com>
2021-08-19 00:33:34 -0300

    estilos de proceso

--- src/assets/jss/process/detail.js
+++ src/assets/jss/process/detail.js
@@ -26,16 +26,21 @@ const styles = {
     justifyContent: 'space-between',
     width: '100%',
   },
-  addNote:{
+  statusBtn:{
     width: '30%',
+    height: '60%',
+    width: 250,
+    placeSelf: 'center',
+    border: '1px solid #c1c1c1',
   },
   input:{
     margin: '24px',
     lineHeight: '1.1',
-    width: '45%'
+    width: '40%'
   },
   name: {
-    fontSize: '1.5em'
+    fontSize: '1.5em',
+    lineHeight: '1em',
   },
   operatorNotes: {
     width: '100%',
@@ -50,12 +55,15 @@ const styles = {
     fontSize: '3em',
     textAlign: 'center'
   },
-  actionBtns: {
+  actionsRow: {
     display: 'flex',
     justifyContent: 'center',
     margin: 'auto',
-    width: '100%',
-    height: '40%',
+    width: '50%',
+    height: '30%',
+  },
+  emptyActionsRow : {
+    width: '50%',
   },
   onWorkingBtns: {
     display: 'flex',
@@ -69,24 +77,69 @@ const styles = {
   },
   primary: {
     color: 'white',
+    height: '60%',
+    width: 250,
+    placeSelf: 'center',
     background: '#52c41a',
-    fontSize: '0.4em',
     '&:hover':{
       color: 'white',
-      background: '#52c41a',
-      fontSize: '0.4em',
+      background: '#52c41a75',
+    }
+  },
+  secondary: {
+    height: '60%',
+    width: 250,
+    margin: '48px 0',
+    placeSelf: 'center',
+    border: '1px solid #c1c1c1',
+    '&:hover':{
     }
   },
   doneHandlerBtn:{
-    fontSize: '0.4em',
-    border: '1px solid #c1c1c1',
-  },
-  allDoneBtn:{
-    fontSize: '0.4em',
+    fontSize: '1em',
     margin: '0 12px',
-    width: '25%',
     border: '1px solid #c1c1c1',
   },
+  fsfasfsfa: {
+    marginTop: 64
+  },
+
+  '@media screen and (max-width: 600px)': {
+    container: {
+      height: 'auto'
+    },
+    row: { 
+      flexDirection: 'column',  
+      width: '100%',
+      border: '1px solid #ededed',
+      marginBottom: 24,
+    },
+    input: {
+      width: '85%' 
+    },
+    statusBtn: {
+      width: '85%',
+      margin: '0 4px',
+      alignSelf: 'center',
+      marginBottom: 24,
+    },
+    actionsRow: {
+      width: '100%',
+      margin: '24px 0',
+    },
+    primary: {
+      alignSelf: 'center',
+    },
+    secondary: {
+      margin: 0
+    },
+    emptyActionsRow : {
+      display: 'none',
+    },
+    fsfasfsfa: {
+      marginTop: 0
+    }
+  }
 }
 
 export default styles;
--- src/components/Process/DetailProcess/index.js
+++ src/components/Process/DetailProcess/index.js
@@ -1,129 +1,107 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
-import styles from 'assets/jss/process/detail'
+import styles from 'assets/jss/process/detail';
 import processService from 'services/processService';
 const useStyles = makeStyles(styles);
 
-function DetailProcess({
-  data,
-  onDrawerClose,
-  onStatusChange,
-  onDoneQuantityChange,
-}){
-  const classes = useStyles();
-  const optionsStatus = useSelector(state => state.appData.status);
-  const lookupstatus = useSelector(state => state.appData.lookupstatus);
-  const {
-    _id: id,
-    name,
-    batchNumber,
-    company,
-    product,
-    observation,
-    quantity,
-    done,
-    status,
-    operatorNotes,
-    startDate,
-  } = data;
+function DetailProcess({ data, onDrawerClose, onStatusChange, onDoneQuantityChange }) {
+	const classes = useStyles();
+	const optionsStatus = useSelector((state) => state.appData.status);
+	const lookupstatus = useSelector((state) => state.appData.lookupstatus);
+	const { _id: id, name, batchNumber, company, product, observation, quantity, done, status, operatorNotes } = data;
 
-  const onStart = () => {
-    processService.startTask({ id })
-    .then()
-    .catch()
-  }
+	const onStart = () => {
+		processService.startTask({ id }).then().catch();
+	};
 
-  return (
-    <Drawer 
-      classes={{
-        paper: classes.drawer,
-      }}
-      anchor="right" 
-      open={Boolean(data)} 
-      onClose={onDrawerClose}>
-        <div className={classes.container}>
-          <div className={classes.info}>
-            <div className={classes.header}>
-              <div className={classes.name}>{name}</div>
-              <div className={classes.batchNumber}>{batchNumber}</div>
-            </div>
-            <div className={classes.row}>
-              <div className={classes.input}>{company}</div>
-              <div className={classes.input}>{product}</div>
-            </div>
-            <div className={classes.row}>
-              {observation && <div className={classes.input}>{observation}</div>}
-              <div className={classes.input}>{`${done} completas de ${quantity}`}</div>
-            </div>
-            <div className={classes.row}>
-              <Autocomplete 
-                label="Estado" 
-                id="status"
-                options={optionsStatus} 
-                defaultValue={{name: lookupstatus[status]}}
-                // onChange={actions.handleInputChange}
-                getOptionSelected={(option, value) => option.name === value.name}
-                getOptionLabel={option => option.name}
-                className={classes.input} 
-                renderInput={(params) => (
-                  <TextField {...params} label="Estado" name="status" />
-                  )}
-                />
-              <TextField 
-                className={classes.operatorNotes} 
-                label="Agregar Notas del operador"
-                name="operatorNotes"
-                multiline
-                defaultValue={operatorNotes}
-                // onChange={actions.handleInputChange}
-                />
-                <Button onClick={onStatusChange} className={classes.addNote}>Cambiar Estado</Button>
-            </div>
-          </div>
-          <Divider />
-          <div className={classes.actionBtns}>
-            {startDate 
-            ? (
-              <div className={classes.onWorkingBtns}>
-                <div className={classes.row}>
-                  <Button className={classes.doneHandlerBtn}>-</Button>
-                  <TextField 
-                    className={classes.doneRegister} 
-                    name="doneRegister"
-                    type="number"
-                    defaultValue={done}
-                    InputProps={{
-                      classes: {
-                        input: classes.resize,
-                      },
-                    }}
-                    // onChange={actions.handleInputChange}
-                    />
-                  <Button className={classes.doneHandlerBtn}>+</Button>
-                  <Button className={classes.allDoneBtn}>Todas</Button>
-                </div>
-                <div className={classes.row}>
-                  <Button onClick={onDoneQuantityChange} fullWidth className={classes.primary}>Registrar</Button>
-                </div>
-              </div>
-            )
-            : (
-              <div className={classes.onWorkingBtns}>
-                <Button onClick={onStart} className={classes.primary}>Iniciar Tarea</Button>
-              </div>
-            )}
-          </div>
+	return (
+		<Drawer
+			classes={{
+				paper: classes.drawer
+			}}
+			anchor="right"
+			open={Boolean(data)}
+			onClose={onDrawerClose}
+		>
+			<div className={classes.container}>
+				<div className={classes.header}>
+					<div className={classes.name}>{name}</div>
+					<div className={classes.batchNumber}>{batchNumber}</div>
+				</div>
+				<div className={classes.row}>
+					<div className={classes.input}>{company}</div>
+					<div className={classes.input}>{product}</div>
+				</div>
+				<div className={classes.row}>
+					{observation && <div className={classes.input}>{observation}</div>}
+					<div className={classes.input}>{`${done} completas de ${quantity}`}</div>
+				</div>
+				<div className={classes.row}>
+					<Autocomplete
+						label="Estado"
+						id="status"
+						options={optionsStatus}
+						defaultValue={{ name: lookupstatus[status] }}
+						// onChange={actions.handleInputChange}
+						getOptionSelected={(option, value) => option.name === value.name}
+						getOptionLabel={(option) => option.name}
+						className={classes.input}
+						renderInput={(params) => <TextField {...params} label="Estado" name="status" />}
+					/>
+					<TextField
+						className={classes.input}
+						label="Agregar Notas del operador"
+						name="operatorNotes"
+						multiline
+						defaultValue={operatorNotes}
+						// onChange={actions.handleInputChange}
+					/>
+					<Button onClick={onStatusChange} className={classes.statusBtn}>
+						Cambiar Estado
+					</Button>
+				</div>
 
-        </div>
-    </Drawer>
-  )
+				<div className={classes.fsfasfsfa}>
+					<div className={classes.row}>
+						<div className={classes.actionsRow}>
+							<Button className={classes.doneHandlerBtn}>-</Button>
+							<TextField
+								className={classes.doneRegister}
+								name="doneRegister"
+								type="number"
+								defaultValue={done}
+								InputProps={{
+									classes: {
+										input: classes.resize
+									}
+								}}
+							/>
+							<Button className={classes.doneHandlerBtn}>+</Button>
+						</div>
+						<div className={classes.actionsRow}>
+							<Button onClick={onDoneQuantityChange} fullWidth className={classes.primary}>
+								Registrar
+							</Button>
+						</div>
+					</div>
+					<div className={classes.row}>
+						<div className={classes.emptyActionsRow} />
+						<div className={classes.actionsRow}>
+							<Button onClick={onDoneQuantityChange} fullWidth className={classes.secondary}>
+								Finalizar
+							</Button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Drawer>
+	);
 }
 
-export default DetailProcess
+export default DetailProcess;
