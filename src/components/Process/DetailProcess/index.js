8d60446bb4e02345d6b01e8fa77169dcc3f5d154
pabloluceroschneider <pabloluceroschneider@gmail.com>
2021-09-11 17:54:30 -0300

    remove useState

--- src/components/Process/DetailProcess/index.js
+++ src/components/Process/DetailProcess/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from 'react';
+import React, { useReducer } from 'react';
 import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
