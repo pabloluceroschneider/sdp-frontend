272ea7bf0a0f66a1b032951aba5b8d16245e07c5
pabloluceroschneider <pabloluceroschneider@gmail.com>
2021-09-19 18:44:12 -0300

    test scope

--- src/helpers/scope.js
+++ src/helpers/scope.js
@@ -1,6 +1,9 @@
 const prod = 'https://api-sdp.herokuapp.com/api/v1'
+const test = 'https://sdp-api-test.herokuapp.com/api/v1'
 const localhost = 'http://localhost:5000/api/v1'
 const ip_localhost = 'http://192.168.0.165:5000/api/v1'
+
+const frontend_prod = 'sistema-de-produccion.vercel.app'
 
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
@@ -16,8 +19,9 @@ const param = {
 const domain = {
   localhost,
   "192.168.0.165": ip_localhost,
+  [frontend_prod]: prod
 }
 
-const scope = param[scopeParam] || domain[hostname] || prod
+const scope = param[scopeParam] || domain[hostname] || test
 
 export default scope
